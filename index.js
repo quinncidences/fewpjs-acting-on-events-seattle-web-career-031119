65d6bd74850829472feeb87a04aedf664747d9a5
quinncidences <qrc.inpa@gmail.com>
2019-04-24 15:53:18 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+var dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.left; // "180px"
+dodger.style.bottom = "0px"; // "0px"
+dodger.style.right;
+dodger.style.up;
+dodger.style.down;
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
